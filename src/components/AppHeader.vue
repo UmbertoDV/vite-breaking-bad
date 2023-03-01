92f629a9c2237951e1f9eab1b214e4cf8957a8f7
UmbertoDV <biccoumby2002@hotmail.com>
2023-03-01 15:04:46 +0100

    add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+// JS
+</script>
+
+<template>
+	<div class="header">
+		<img
+			src="https://png2.cleanpng.com/sh/465c635f0b12022e765539ebd231df0e/L0KzQYm3U8MxN6lqfZH0aYP2gLBuTgl2NZhuRdHxLYTrdX76gfNzbZUye9N7ZIOwica0hBkud5kyjORqZHnxd37qggJlNZ50huV9ZYL2Pbr1g702aZNpftU5OHOzdbToUL44PWI7TaoANEG4QoO7UMAzPmU8UaI7LoDxd1==/kisspng-yu-gi-oh-the-sacred-cards-yu-gi-oh-trading-card-monsters-inc-5abdfc08c0eca0.7516585415224002647902.png"
+			alt="Yu-Gi-Oh LOGO"
+		/>
+		<h1 class="fw-bold my-3">Yu-Gi-Oh API</h1>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+//SCSS
+@use "../assets/scss/partials/mixins" as *;
+.header {
+	@include flex("vertical");
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUserSecret, faBook } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserSecret, faBook);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
